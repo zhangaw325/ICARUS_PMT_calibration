6fd6583178ccb4873ce41a6953d5d689607fed93
Kees Benkendorfer <keesbenkendorfer@Keess-MacBook-Pro.local>
2019-06-13 10:52:40 -0400

    Updated to display each PMT on its own canvas

--- FitChargeDistributions.C
+++ FitChargeDistributions.C
@@ -1,4 +1,5 @@
 #include "TH1.h"
+#include "TCanvas.h"
 
 //#include "Fit_Spe.C" // the fit function is defined in this file
 
@@ -33,40 +34,40 @@ void FitChargeDistributions(string pmtRow,
     cout << rtfilenames[i] << endl;
   }
   const int NCH = 4; // 4 PMTs
-  /*
-  string PMTIDstr[NCH]={
-                    "A12_PMT1",
-                    "A12_PMT2",
-                    "A12_PMT3",
-                    "A12_PMT4"
-                    };
-                    */
-  int rebinfactor[NCH]={1, 1, 1, 1}; // histograms need some rebin
+ 
+  int rebinfactor[NCH]={1, 1, 1, 1}; // rebin histograms
   double fitbeginch[NCH]={1.0,1.0,1.0,1.0};
+  double fitend = 60;
 
   string outnameroot = strchimney + strpmt + "gain.root";
   string outnametxt = strchimney + strpmt + "gain_fit.txt";
   TFile* outROOTfile = new TFile(outnameroot.c_str(),"recreate");  
   fstream foutFit(outnametxt.c_str(),ios::out);
-  TH1F* hCharge[NCH]; // histograms
+  TH1F* hCharge[3]; // histograms for each canvas
   TCanvas* c[3];
   char tempname[100];
 
-  // Read each of the three output ROOT files
-  for(int i=0; i<3; i++){
-    TFile* f = new TFile(rtfilenames[i].c_str(),"read");
-    sprintf(tempname,"c_%d",i);
-    c[i] = new TCanvas(tempname,rtfilenames[i].c_str(),1200,900); // generate canvas
-    c[i]->Divide(2,2); // divide canvas into four panels
+  TFile* files[3];
+  // Read each of the three data ROOT files and save them to files[]
+  for(int i = 0; i < 3; i++){
+    files[i] = new TFile(rtfilenames[i].c_str(),"read");
+  }
 
-    // Generate histograms and fits for each of the four PMTs in each ROOT file
-    for(int j=0; j<NCH; j++){
-      c[i]->cd(j+1); // switch to the proper panel
-      sprintf(tempname,"Results/FinalCharge_%d",j); // store string "Results/FinalCharge_%d" in tempname
-      hCharge[j] = (TH1F*)f->Get(tempname);
-      hCharge[j]->Rebin(rebinfactor[j]);
+  // Generate 4 canvases and plot the PMT histograms on them
+  for(int i = 0; i < 4; i++){
+    sprintf(tempname, "c_%d",i);
+    sprintf(canvasTitle,strchimney + "%d",i); // generate canvas title
+    c[i] = new TCanvas(tempname,canvasTitlev.c_str(),1400,600); // generate canvas
+    c[i]->Divide(3); // divide canvas into 3 pads along the width
+
+    // Plot the histograms
+    for(int j = 0; j<3; j++){
+      c[i]->cd(j+1); // switch pads
+      sprintf(tempname,"Results/FinalCharge_%d",i); // store string "Results/FinalCharge_%d" in tempname
+      hCharge[j] = (TH1F*)f[j]->Get(tempname); // read histogram data from the ROOT file
+      hCharge[j]->Rebin(rebinfactor[i]);
       hCharge[j]->SetXTitle("Charge in pC, (10^{7} electrons = 1.6 pC)");
-      hCharge[j]->Draw(); 
+      hCharge[j]->Draw();
 
       // Set initial fit parameters
       Fideal->SetParameter(1,1.6);
@@ -77,39 +78,19 @@ void FitChargeDistributions(string pmtRow,
       Fideal->SetParLimits(2,0.1,10);
       Fideal->SetParLimits(3,0.1,20000);
 
-      //if(j==1) Fideal->SetParameter(3,1000);
-
-      //if(j==1 && i==2) fitbeginch[j] = 1.0;
-      
-      //hCharge[j]->GetXaxis()->SetRangeUser(0, hCharge[j]->GetMean()*5.0);
-      //double fitbegin = 0;
-      //if(j==2) fitbegin = 0.8;
-      //if(j==3) fitbegin = 4;
-
-      double fitend = 50;
-
       // Iteratively fit more than once
       for(int k=0; k<2;k++){
-        //if(j!=1)
-           //hCharge[j]->Fit("Fideal","RQ","",fitbeginch[j],hCharge[j]->GetMean()*5.0);
-        //else hCharge[j]->Fit("Fideal","RQ","",fitbeginch[j],fitend);
         hCharge[j]->Fit("Fideal","RQ","",fitbeginch[j],fitend); // Fit the histogram
         Fideal->GetParameters(par);
         Fideal->SetParameters(par); // Set fit parameters for next iteration
       }
 
-      // if(j!=1) 
-      //     hCharge[j]->GetXaxis()->SetRangeUser(0, hCharge[j]->GetMean()*5.0);
-      // else{
-      //     hCharge[j]->GetXaxis()->SetRangeUser(0, fitend);
-      //     hCharge[j]->GetYaxis()->SetRangeUser(0, 200);
-      // }
-
       hCharge[j]->GetXaxis()->SetRangeUser(0, fitend); // Set axes
 
+      // write parameters to output txt file
       Fideal->GetParameters(par);
       //parerr = Fideal->GetParErrors();
-      foutFit<<"voltage\t"<<voltagestr[i]<<"\tchID\t"<<j
+      foutFit<<"voltage\t"<<voltagestr[j]<<"\tchID\t"<<i
              <<"\t"<<par[0]<<"\t"<<Fideal->GetParError(0)
              <<"\t"<<par[1]<<"\t"<<Fideal->GetParError(1)
              <<"\t"<<par[2]<<"\t"<<Fideal->GetParError(2)
@@ -119,14 +100,66 @@ void FitChargeDistributions(string pmtRow,
              <<endl;
     }
 
-    //c[i]->Update();
+    // write results to output ROOT file
     outROOTfile->cd();
     c[i]->Write();
-    //f->Close();
-    //if(i>0) break;
   }
+
+  // Read each of the three output ROOT files and generate 3 canvases
+  // for(int i=0; i<3; i++){
+  //   TFile* f = new TFile(rtfilenames[i].c_str(),"read");
+  //   sprintf(tempname,"c_%d",i);
+  //   c[i] = new TCanvas(tempname,rtfilenames[i].c_str(),1200,900); // generate canvas
+  //   c[i]->Divide(2,2); // divide canvas into four panels
+
+  //   // Generate histograms and fits for each of the four PMTs in each ROOT file
+  //   for(int j=0; j<NCH; j++){
+  //     c[i]->cd(j+1); // switch to the proper panel
+  //     sprintf(tempname,"Results/FinalCharge_%d",j); // store string "Results/FinalCharge_%d" in tempname
+  //     hCharge[j] = (TH1F*)f->Get(tempname);
+  //     hCharge[j]->Rebin(rebinfactor[j]);
+  //     hCharge[j]->SetXTitle("Charge in pC, (10^{7} electrons = 1.6 pC)");
+  //     hCharge[j]->Draw(); 
+
+  //     // Set initial fit parameters
+  //     Fideal->SetParameter(1,1.6);
+  //     Fideal->SetParameter(2,1.6*0.4);
+  //     Fideal->SetParameter(3,hCharge[j]->Integral());
+  //     Fideal->SetParLimits(0,0.1,100);
+  //     Fideal->SetParLimits(1,0.5,10);
+  //     Fideal->SetParLimits(2,0.1,10);
+  //     Fideal->SetParLimits(3,0.1,20000);
+
+  //     // Iteratively fit more than once
+  //     for(int k=0; k<5;k++){
+  //       hCharge[j]->Fit("Fideal","RQ","",fitbeginch[j],fitend); // Fit the histogram
+  //       Fideal->GetParameters(par);
+  //       Fideal->SetParameters(par); // Set fit parameters for next iteration
+  //     }
+
+  //     hCharge[j]->GetXaxis()->SetRangeUser(0, fitend); // Set axes
+
+  //     Fideal->GetParameters(par);
+  //     //parerr = Fideal->GetParErrors();
+  //     foutFit<<"voltage\t"<<voltagestr[i]<<"\tchID\t"<<j
+  //            <<"\t"<<par[0]<<"\t"<<Fideal->GetParError(0)
+  //            <<"\t"<<par[1]<<"\t"<<Fideal->GetParError(1)
+  //            <<"\t"<<par[2]<<"\t"<<Fideal->GetParError(2)
+  //            <<"\t"<<Fideal->GetChisquare()
+  //            <<"\t"<<Fideal->GetNDF()
+  //            <<"\t"<<Fideal->GetProb()
+  //            <<endl;
+  //   }
+
+  //   //c[i]->Update();
+  //   outROOTfile->cd();
+  //   c[i]->Write();
+  //   //f->Close();
+  //   //if(i>0) break;
+  // }
+
+  // close output ROOT file
   outROOTfile->Close();
-  
 }
 
 
