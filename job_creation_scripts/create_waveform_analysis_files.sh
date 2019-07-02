ae013d270199597b9a70fb9950fcd7ce03bb3a61
Kees Benkendorfer <keesbenkendorfer@gmail.com>
2019-07-02 16:19:28 -0400

    Reorganized condor jobs

--- job_creation_scripts/create_waveform_analysis_files.sh
+++ job_creation_scripts/create_waveform_analysis_files.sh
@@ -4,8 +4,12 @@ echo "Beginning creation of waveform analysis files"
 
 i=0;
 while read line; do
-	echo $line > ${i}.WaveformDirectories.txt
-	echo "$line > ${i}.WaveformDirectories.txt"
+	mkdir ${i}_files
+	echo $line > ${i}_files/${i}.WaveformDirectories.txt
+	echo "$line > ${i}_files/${i}.WaveformDirectories.txt"
+	cp analyze_cl.sh ${i}_files/
+	cp ProcessData_cl_threshold.py ${i}_files/
+	cp fix_led_names.sh ${i}_files/
 	((i++))
 done < WaveformDirectories.txt
 
