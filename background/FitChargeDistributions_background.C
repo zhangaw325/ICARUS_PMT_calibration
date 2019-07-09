156b1c8cdd99845be77e286b3b02c0e5ed6aeac2
Kees Benkendorfer <keesbenkendorfer@Keess-MacBook-Pro-5.local>
2019-07-09 16:18:07 -0400

    Added preliminarily functional background fitting code

--- background/FitChargeDistributions_background.C
+++ background/FitChargeDistributions_background.C
@@ -13,6 +13,10 @@ double PI = TMath::Pi();
 
 double IdealResponse(double *x,double *par);
 double RealResponse(double *x, double *par);
+Double_t iGnE(Double_t x, Double_t q0, Double_t sigma0, 
+              Double_t q1, Double_t sigma1, Double_t a, Double_t n);
+Double_t Gn(Double_t x, Double_t q0, Double_t sigma0, Double_t q1, 
+            Double_t sigma1, Double_t n);
 Double_t truncatedMean(TH1 *hist, int n_iterations, int n_rejection_stddevs = 3);
 
 void FitChargeDistributions_background(std::string pmtRow,
@@ -20,16 +24,16 @@ void FitChargeDistributions_background(std::string pmtRow,
 			    int volt1, int volt2, int volt3, bool led){
 
   const int NCH = 4; // 4 PMTs
-  const int NPAR = 7;
+  const int NPAR = 8;
   // histogram and fit options
-  int rebinfactor[NCH]={5, 5, 5, 5}; // rebin histograms
-  double fitbeginch[NCH]={1.0,1.0,1.0,1.0}; // fit start locations
-  double fitendch[NCH] = {60,60,60,60}; // fit end locations
+  int rebinfactor[NCH]={1, 1, 1, 1}; // rebin histograms
+  double fitbeginch[NCH]={-0.75,-0.75,-0.75,-0.75}; // fit start locations
+  double fitendch[NCH] = {30,30,30,30}; // fit end locations
 
   // the function to be used to do fit
   gStyle->SetOptFit(1111);
   TF1* Freal = new TF1("Freal",RealResponse, 0, 500, NPAR);
-  Freal->SetParNames("meanNpe","pedestalPeak","pedestalSigma","spePeak","speSigma","w","alpha");
+  Freal->SetParNames("meanNpe","pedestalPeak","pedestalSigma","spePeak","speSigma","w","alpha","amplitude");
   Freal->SetLineColor(2); Freal->SetLineStyle(1);
   double par[NPAR];
   double parerr[NPAR];
@@ -64,6 +68,9 @@ void FitChargeDistributions_background(std::string pmtRow,
     files[i] = new TFile(rtfilenames[i].c_str(),"read");
   }
 
+  // set call limits
+  //ROOT::Math::MinimizerOptions::SetDefaultMaxFunctionCalls(5000);
+
   // Generate 4 canvases and plot the PMT histograms on them
   for(int i = 0; i < 4; i++){
     sprintf(tempname, "c_%d",i);
@@ -86,28 +93,29 @@ void FitChargeDistributions_background(std::string pmtRow,
       //Freal->SetParameter(1,1.6);
       //Freal->SetParameter(2,1.6*0.4);
       //Freal->SetParameter(3,hCharge[j]->Integral());
-      Freal->SetParLimits(0,0.1,100);
-      Freal->SetParLimits(1,0.1,10);
-      Freal->SetParLimits(2,0.01,10);
-      Freal->SetParLimits(3,0.1, 10);
-      Freal->SetParLimits(4, 0.01, 10);
-      Freal->SetParLimits(5, 0, 1);
-      Freal->SetParLimits(6, 0, 100);
+      Freal->SetParLimits(0,1,30);
+      Freal->SetParLimits(1,-1,1);
+      Freal->SetParLimits(2,0.0001,1);
+      Freal->SetParLimits(3,0.1, 4);
+      Freal->SetParLimits(4, 0.1, 4);
+      Freal->SetParLimits(5, 0.00001, 1);
+      Freal->SetParLimits(6, 0.00000001, 1);
+      Freal->SetParLimits(7, 1, 100000);
 
-      //Double_t hist_mean = truncatedMean(hCharge[j],10);
-      //Freal->SetParameter(1,hist_mean);
+      Double_t hist_mean = truncatedMean(hCharge[j],10);
+      Freal->SetParameter(0,hist_mean);
+      Freal->SetParameters(hist_mean, 0.005, 0.08, 1.2, 1, 0.5, 0.003, 2000);
 
       // Iteratively fit more than once
-      for(int k=0; k<2;k++){
-        hCharge[j]->Fit("Freal","RQ","",fitbeginch[i],fitendch[i]); // Fit the histogram
-        Freal->GetParameters(par);
-        Freal->SetParameters(par); // Set fit parameters for next iteration
-      }
+      // for(int k=0; k<2;k++){
+      //   hCharge[j]->Fit("Freal","RQ","",fitbeginch[i],fitendch[i]); // Fit the histogram
+      //   Freal->GetParameters(par);
+      //   Freal->SetParameters(par); // Set fit parameters for next iteration
+      // }
       
       //hCharge[j]->Fit("expo","","",fitbeginch[i],fitendch[i]);
-      hCharge[j]->Fit("Freal","","",fitbeginch[i],fitendch[i]);
-      
-      hCharge[j]->GetXaxis()->SetRangeUser(0, fitendch[i]); // Set axes
+      hCharge[j]->Fit("Freal","LM","",fitbeginch[i],fitendch[i]);
+      hCharge[j]->GetXaxis()->SetRangeUser(fitbeginch[i], fitendch[i]); // Set axes
 
       // write parameters to output txt file
       Freal->GetParameters(par);
@@ -145,38 +153,58 @@ double IdealResponse(double *x,double *par){
 }
 
 double RealResponse(double *x, double *par){
-	double mu = par[0];
-	double q0 = par[1];
-	double sigma0 = par[2];
-	double q1 = par[3];
-	double sigma1 = par[4];
-	double w = par[5];
-	double a = par[6];
+  double mu = par[0];
+  double q0 = par[1];
+  double sigma0 = par[2];
+  double q1 = par[3];
+  double sigma1 = par[4];
+  double w = par[5];
+  double a = par[6];
+  double amplitude = par[7];
 
-	Double_t sum = 0;
+  Double_t sum = 0;
 
-	for(Int_t n = 1; n < 50; n++){
-		Double_t qn = q0 + n*q1;
-		Double_t sigman = TMath::Sqrt(sigma0*sigma0 + n*sigma1*sigma1);
-		Double_t sign = 1;
-		if(x[0] - qn - sigman*sigman*a > 0){
-			sign = 1;
-		} else {
-			sign = -1;
-		}
+  for(Int_t n = 0; n < 50; n++){
+    sum += (TMath::Power(mu, n)*TMath::Exp(-1.0*mu)/TMath::Factorial(n))
+          * ((1 - w)*Gn(x[0], q0, sigma0, q1, sigma1, n) 
+          + w*iGnE(x[0], q0, sigma0, q1, sigma1, a, n));
+  }
 
-		Double_t iGnE = (a/2)*TMath::Exp(-1.0*a*(x[0] - qn - sigman*sigman))
-						*(TMath::Erf(TMath::Abs(q0 - qn - sigman*sigman*a)/sigman*TMath::Sqrt2())
-						+ TMath::Erf(TMath::Abs(x[0] - qn - sigman*sigman*a)/sigman*TMath::Sqrt2()));
+  return amplitude*sum;
+}
 
-		Double_t Gn = TMath::Exp(-1.0*(x[0] - n*q1)*(x[0] - n*q1)/(2*n*sigma1*sigma1))
-						/(sigma1*TMath::Sqrt(2*TMath::Pi()*n));
+Double_t iGnE(Double_t x, Double_t q0, Double_t sigma0, 
+              Double_t q1, Double_t sigma1, Double_t a, Double_t n){
 
-		sum += (TMath::Power(mu, n)*TMath::Exp(-1.0*mu)/TMath::Factorial(n))
-				* ((1 - w)*Gn + w*iGnE);
-	}
+  Double_t qn = q0 + n*q1;
+  Double_t sigman = TMath::Sqrt(sigma0*sigma0 + n*sigma1*sigma1);
+  Double_t sign = 1;
+  if(x - qn - sigman*sigman*a > 0){
+    sign = 1;
+  } else if(x - qn - sigman*sigman*a == 0){
+    sign = 0;
+  } else {
+    sign = -1;
+  }
 
-	return sum;
+  Double_t iGnE = (a/2)*TMath::Exp(-1.0*a*(x - qn - a*sigman*sigman))
+            *(TMath::Erf(TMath::Abs(q0 - qn - sigman*sigman*a)/(sigman*TMath::Sqrt2()))
+            + sign*TMath::Erf(TMath::Abs(x - qn - sigman*sigman*a)/(sigman*TMath::Sqrt2())));
+
+  return iGnE;
+}
+
+Double_t Gn(Double_t x, Double_t q0, Double_t sigma0, Double_t q1, Double_t sigma1, Double_t n){
+  Double_t Gn = 0;
+  if(n == 0){
+    Gn = TMath::Exp(-1.0*(x - q0)*(x - q0)/(2*sigma0*sigma0))
+           /(sigma0*TMath::Sqrt(2*TMath::Pi()));
+  } else {
+    Gn = TMath::Exp(-1.0*(x - n*q1)*(x - n*q1)/(2*n*sigma1*sigma1))
+            /(sigma1*TMath::Sqrt(2*TMath::Pi()*n));
+  }
+
+  return Gn;
 }
 
 Double_t truncatedMean(TH1 *hist, int n_iterations, int n_rejection_stddevs){
@@ -191,7 +219,7 @@ Double_t truncatedMean(TH1 *hist, int n_iterations, int n_rejection_stddevs){
     mean = hist->GetMean(1);
     stddev = hist->GetMeanError();
 
-    std::cout << mean;
+    // std::cout << mean;
 
     // Truncate
     Double_t new_start = mean - n_rejection_stddevs * stddev;
