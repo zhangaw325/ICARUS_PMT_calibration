343fcf301fef3b9986e2c2797a8758187c14fb1f
Kees Benkendorfer <keesbenkendorfer@gmail.com>
2019-07-15 11:51:14 -0400

    Made fit repeatedly

--- GainVoltage.C
+++ GainVoltage.C
@@ -58,8 +58,8 @@ void GainVoltage(string chimney){
     while (input_file >> p >> v >> g >> ge){
       if (p == pmt_num + 1){
         voltage_raw[num_data_points] = v;
-        gain_raw[num_data_points] = g*pow(10,7);
-        gain_error_raw[num_data_points] = ge*pow(10,7);
+        gain_raw[num_data_points] = g*TMath::Power(10,7);
+        gain_error_raw[num_data_points] = ge*TMath::Power(10,7);
         num_data_points++;
       }
     }
@@ -116,35 +116,41 @@ void GainVoltage(string chimney){
 
     // Create graph of data
     TGraph* data = new TGraphErrors(num_data_points, voltage, gain, voltage_error, gain_error);
-    string title =  "PMT " + chimney + "_" + to_string(pmt_num + 1) + " gain vs voltage (log);"
+    string title =  "PMT " + chimney + "_" + to_string(pmt_num) + " gain vs voltage (log);"
                     + "log(voltage [V]);"
                     + "log(gain)";
     data->SetTitle(title.c_str());
     data->SetMarkerStyle(kOpenSquare);
 
     // Fit
-    fit->SetParameters(0.003,0.33);
-    data->Fit("fit");
+    fit->SetParameters(-30,7);
+    cout << "Fitting " << pmt_num + 1 << endl;
+    data->Fit("fit","ME");
+    for(int j = 0; j < 9; j++){
+	fit->GetParameters(par);
+	fit->SetParameters(par[0],par[1]);
+	data->Fit("fit","ME");
+    }
+    //data->Fit("fit","E");
     gStyle->SetOptFit();
     fit->GetParameters(par);
     Double_t constant = par[0];
     Double_t exponent = par[1];
-    Double_t amplitude = TMath::Exp(constant); 
-    // spacer lines
+    Double_t amplitude = TMath::Exp(constant);
+    //fout << "PMT" << "\t" << pmt_num + 1 
     for(int j = 0; j < 2; j++){
-      fout  << "--" << ",\t" << "--"
-            << ",\t" << "--" << ",\t" << "--"
-            << ",\t" << "--"
-            << ",\t" << "--"
-            << ",\t" << "--"
+      fout  << "--" << "," << "--"
+            << "," << "--" << "," << "--"
+            << "," << "--"
+            << "," << "--"
+            << "," << "--"
             << endl;
     }
-    //fout << "PMT" << "\t" << pmt_num + 1
-    fout << constant <<",\t"<<fit->GetParError(0)
-         << ",\t" << exponent <<",\t"<<fit->GetParError(1)
-         << ",\t" << fit->GetChisquare()
-         << ",\t" << fit->GetNDF()
-         << ",\t" << fit->GetProb()
+    fout << constant <<","<<fit->GetParError(0)
+         << "," << exponent <<","<<fit->GetParError(1)
+         << "," << fit->GetChisquare()
+         << "," << fit->GetNDF()
+         << "," << fit->GetProb()
          <<endl;
 
     // Plot gain and voltage
@@ -159,7 +165,7 @@ void GainVoltage(string chimney){
     TGraph *dataLinear = new TGraphErrors(  num_data_points, voltage_nolog, 
                                         gain_nolog, voltage_error_nolog, 
                                         gain_error_nolog);
-    title = "PMT " + chimney + "_" + to_string(pmt_num + 1) + " gain vs voltage (linear);"
+    title = "PMT " + chimney + "_" + to_string(pmt_num) + " gain vs voltage (linear);"
                    + "voltage [V];"
                    + "gain";
     dataLinear->SetTitle(title.c_str());
