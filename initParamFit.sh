eec2db126662769f3e3b71c515cc38aeed3c0b3b
Hannah Kim <hhk8rj@virginia.edu>
2019-08-12 12:02:05 -0400

    Commented and cleaned code

--- initParamFit.sh
+++ initParamFit.sh
@@ -1,6 +1,6 @@
-# Runs the initial parameter fit and creates a compiled .pdf file
-# Assumes that there is only one .csv file in the given directory,
-#  which holds the initial parameter information
+# - Runs the initial parameter fit and creates a compiled .pdf file
+# - Assumes that there is only one .csv file in the given directory,
+#   which holds the initial parameter information
 
 #!/usr/bin/env bash
 
@@ -14,10 +14,13 @@ if [[ $input == "Y" || $input == "y" ]]; then
     #file name formatting
     csvName=`ls *.csv | head -1`
     pdfName="${csvName/csv/pdf}"   
+# Loads root and runs the FitChargeDistributions_InitParam to create individual
+# pdfs for all the PMTs
     ROOTSYS=/cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-ubuntu18-gcc73-opt PATH=$ROOTSYS/bin:$PATH root -l -b <<EOF
         .x FitChargeDistributions_InitParam.C("$csvName");
         .q;
 EOF
+# Consolidates all the pdfs into a single pdf file
     pdfunite *.pdf $pdfName
     echo "Analysis finished."
     echo "Output to: $pdfName"
